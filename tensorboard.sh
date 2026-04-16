af2fe00a82d82c7b64f748d9f3ead9fe32cf9f30
Tom Havlik <thavlik@protonmail.com>
2026-04-16 09:56:21 -0400

    maintain shell scripts since tensorflow/tensorflow dropped tensorboard for some reason

--- tensorboard.sh
+++ tensorboard.sh
@@ -5,7 +5,7 @@ LOGDIR="${1:-./runs_vae}"
 PORT="${2:-6006}"
 
 docker run --rm -it \
-    -p "${PORT}:6006" \
-    -v "$(realpath "${LOGDIR}"):/logs:ro" \
-    tensorflow/tensorflow:latest \
-    tensorboard --logdir=/logs --bind_all
+  -p "${PORT}:6006" \
+  -v "$(realpath "${LOGDIR}"):/logs:ro" \
+  hkube/tensorboard \
+  tensorboard --logdir=/logs --bind_all
